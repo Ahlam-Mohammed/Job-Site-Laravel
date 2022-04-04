76e5d5709b2dbb8525587c50b20d57030d9ca5ff
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-04-04 23:58:09 +0300

    Migration: migrate tables

---
+++ database/migrations/2022_04_04_205004_create_social_media_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_media', function (Blueprint $table) {
+            $table->id();
+            $table->string('facebook_link')->nullable();
+            $table->string('twitter_link')->nullable();
+            $table->string('linkedin_link')->nullable();
+            $table->string('whatsapp_link')->nullable();
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_media');
+    }
+};
